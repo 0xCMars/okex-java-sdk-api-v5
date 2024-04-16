24fe1a3906ca71c044b1eba4a148dd38e2eed3f8
MarsChenSE <MarsChenSE@gmail.com>
2024-04-16 13:24:14 +0800

    update pom and add priDemo

--- src/main/java/com/okex/open/api/websocket/WebSocketConfig.java
+++ src/main/java/com/okex/open/api/websocket/WebSocketConfig.java
@@ -8,9 +8,9 @@ public class WebSocketConfig {
     private static final String SERVICE_URL = "wss://ws.okex.com:8443/ws/v5/private";
 
     // 实盘api key
-    private static final String API_KEY = "99306305-7de4-4c57-bf70-2ef835431182";
-    private static final String SECRET_KEY = "AFEC9D50AB0E5EC6CAF88DAA425B257A";
-    private static final String PASSPHRASE = "m55AVzi@E9un";
+    private static final String API_KEY = "xxxx";
+    private static final String SECRET_KEY = "xxxx";
+    private static final String PASSPHRASE = "xxxx";
 
     public static String getApiKey() {
         return API_KEY;
