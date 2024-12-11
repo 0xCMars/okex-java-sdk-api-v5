c057436f387b7558b6cf37dce57c90b8c8fedb95
tonyho321 <valdisho@gmail.com>
2024-12-11 13:57:56 +0800

    fix websocket urls

--- src/main/java/com/okex/open/api/websocket/WebSocketConfig.java
+++ src/main/java/com/okex/open/api/websocket/WebSocketConfig.java
@@ -2,10 +2,10 @@ package com.okex.open.api.websocket;
 
 public class WebSocketConfig {
     //ws  模拟盘
-//    private static final String SERVICE_URL = "wss://ws.okex.com:8443/ws/v5/private?brokerId=9999";
+//    private static final String SERVICE_URL = "wss://ws.okx.com:8443/ws/v5/private?brokerId=9999";
 
     //ws  实盘
-    private static final String SERVICE_URL = "wss://ws.okex.com:8443/ws/v5/private";
+    private static final String SERVICE_URL = "wss://ws.okx.com:8443/ws/v5/private";
 
     // 实盘api key
     private static final String API_KEY = "xxxx";
--- src/test/java/com/okex/open/api/test/ws/publicChannel/config/WebSocketConfig.java
+++ src/test/java/com/okex/open/api/test/ws/publicChannel/config/WebSocketConfig.java
@@ -2,7 +2,7 @@ package com.okex.open.api.test.ws.publicChannel.config;
 
 public class WebSocketConfig {
     //ws  模拟盘
-//    private static final String SERVICE_URL = "wss://ws.okex.com:8443/ws/v5/public?brokerId=9999";
+//    private static final String SERVICE_URL = "wss://ws.okx.com:8443/ws/v5/public?brokerId=9999";
     //ws  实盘
     private static final String SERVICE_URL = "wss://wsaws.okx.com:8443/ws/v5/public";
 
